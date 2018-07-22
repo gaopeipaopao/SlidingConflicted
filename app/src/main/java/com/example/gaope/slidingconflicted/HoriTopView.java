e23919812555b96e20d0c8f3c78490823b51f62f
gaopei <gaopeigpgp@126.com>
2018-07-22 10:16:03 +0800

    内部拦截法解决类似于ViewPager嵌套ScrollView的不同方向的滑动冲突

--- app/src/main/java/com/example/gaope/slidingconflicted/HoriTopView.java
+++ app/src/main/java/com/example/gaope/slidingconflicted/HoriTopView.java
@@ -77,53 +77,67 @@ public class HoriTopView extends ViewGroup {
     }
 
     //外部拦截法解决类似于ViewPager嵌套ScrollView的不同方向的滑动冲突
+//    @Override
+//    public boolean onInterceptTouchEvent(MotionEvent ev) {
+//
+//        x = (int) ev.getX();
+//        y = (int) ev.getY();
+//        switch (ev.getAction()){
+//            case MotionEvent.ACTION_DOWN:
+//                indownX = x;
+//                indownY = y;
+//                return false;
+//            case MotionEvent.ACTION_MOVE:
+//                int dx = (int) (x - indownX);
+//                int dy = (int) (y - indownY);
+//                Log.d(TAG,"aax:"+x);
+//                Log.d(TAG,"aay:"+y);
+//                Log.d(TAG,"aaindownx:"+indownX);
+//                Log.d(TAG,"aaindowny:"+indownY);
+//                Log.d(TAG,"aadx:"+Math.abs(dx));
+//                Log.d(TAG,"aady:"+dy);
+//                indownX = x;
+//                indownY = y;
+//                if((Math.abs(dx) > Math.abs(dy)) ){
+//                    Log.d("aaa",TAG);
+//                    return true;
+//                }else {
+//                    return false;
+//                }
+//        }
+//
+//        return super.onInterceptTouchEvent(ev);
+//    }
+
+    //内部拦截法解决类似于ViewPager嵌套ScrollView的不同方向的滑动冲突
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-
-        x = (int) ev.getX();
-        y = (int) ev.getY();
-        switch (ev.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                indownX = x;
-                indownY = y;
-                return false;
-            case MotionEvent.ACTION_MOVE:
-                int dx = (int) (x - indownX);
-                int dy = (int) (y - indownY);
-                Log.d(TAG,"aax:"+x);
-                Log.d(TAG,"aay:"+y);
-                Log.d(TAG,"aaindownx:"+indownX);
-                Log.d(TAG,"aaindowny:"+indownY);
-                Log.d(TAG,"aadx:"+Math.abs(dx));
-                Log.d(TAG,"aady:"+dy);
-                indownX = x;
-                indownY = y;
-                if((Math.abs(dx) > Math.abs(dy)) ){
-                    Log.d("aaa",TAG);
-                    return true;
-                }else {
-                    return false;
-                }
+        if (ev.getAction() == MotionEvent.ACTION_DOWN){
+            return false;
+        } else {
+            return true;
         }
-
-        return super.onInterceptTouchEvent(ev);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
         int dx;
+        if (x == 0){
+            x = (int) event.getRawX();
+        }
         downX = x;
         Log.d(TAG,"ccdownx:" + downX);
 
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
+                Log.d(TAG,"nnnn");
                 downX = x1;
                 return true;
             case MotionEvent.ACTION_MOVE:
                 x = (int) event.getRawX();
                 dx =  downX - x;
-
+                downX = x;
                 Log.d(TAG,"ccx:" + x );
                 Log.d(TAG,"ccdx:" + dx );
 
@@ -154,6 +168,9 @@ public class HoriTopView extends ViewGroup {
                 int dxx = (int) (target * getWidth()) - getScrollX();
                 scroller.startScroll(getScrollX() , 0 , dxx , 0);
                 invalidate();
+                downX = 0;
+                x = 0;
+                break;
         }
         return super.onTouchEvent(event);
     }
--- app/src/main/java/com/example/gaope/slidingconflicted/TwoScrollView.java
+++ app/src/main/java/com/example/gaope/slidingconflicted/TwoScrollView.java
@@ -2,6 +2,7 @@ package com.example.gaope.slidingconflicted;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.ScrollView;
 
@@ -11,15 +12,47 @@ import android.widget.ScrollView;
 
 public class TwoScrollView extends ScrollView {
 
+    private static final String TAG = "TwoScrollView";
+    private int indownX;
+    private int indownY;
+    private int x;
+    private int y;
+
     public TwoScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    //内部拦截法
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
+        Log.d(TAG,"indownx1:"+indownX);
+        Log.d(TAG,"indowny1:"+indownY);
+        x = (int) ev.getX();
+        y = (int) ev.getY();
+        int dx;
+        int dy;
+
         switch (ev.getAction()){
             case MotionEvent.ACTION_DOWN:
+                Log.d(TAG,"cacaca");
+                getParent().requestDisallowInterceptTouchEvent(true);
                 return true;
+            case MotionEvent.ACTION_MOVE:
+                dx = (int) (x - indownX);
+                dy = (int) (y - indownY);
+                Log.d(TAG,"x:"+x);
+                Log.d(TAG,"y:"+y);
+                Log.d(TAG,"indownx:"+indownX);
+                Log.d(TAG,"indowny:"+indownY);
+                Log.d(TAG,"dx:"+Math.abs(dx));
+                Log.d(TAG,"dy:"+dy);
+                indownX = x;
+                indownY = y;
+                if((Math.abs(dx) > Math.abs(dy)) ){
+                    Log.d("aaa",TAG);
+                    getParent().requestDisallowInterceptTouchEvent(false);
+                }
+                break;
         }
         return super.onTouchEvent(ev);
     }
